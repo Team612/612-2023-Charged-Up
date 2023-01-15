f4b1cfb0c192a443497e0ccf0242594cdac29b68
Abhinaya Karthana <57462435+AbeeK16@users.noreply.github.com>
2023-01-15 18:41:23 -0500

    changed threads

--- vision/detection.py
+++ vision/detection.py
@@ -7,7 +7,7 @@ vid = cv2.VideoCapture(0)
 
 at_detector = Detector(
     families="tag16h5", 
-    nthreads=15,
+    nthreads=1,
     quad_sigma=0.0, 
     refine_edges=1,
     decode_sharpening=0.25,
