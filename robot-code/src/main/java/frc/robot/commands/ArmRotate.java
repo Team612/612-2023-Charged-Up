20230d19ac0862feabceadf2148583625981a20d
vision <chantilly.612.programming@gmail.com>
2023-03-02 19:29:45 -0500

    Co-authored-by: Vik-02 <Vik-02@users.noreply.github.com> Co-authored-by: Supersmile13 <Supersmile13@users.noreply.github.com> Co-authored-by: Shruthi Mandava <Shruthi-M5@users.noreply.github.com>

--- robot-code/src/main/java/frc/robot/commands/ArmRotate.java
+++ robot-code/src/main/java/frc/robot/commands/ArmRotate.java
@@ -1,14 +1,24 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
+import frc.robot.Constants.DrivetrainConstants;
+
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
+// In Team 612, we are safe as one. We will not tolerate laziness.
 
 public class ArmRotate extends CommandBase {
 
-    //that looks like a major blunder ngl
     private final Arm m_arm;
     private final double m_endAngle;
-
+    
     public ArmRotate(Arm arm, double endAngle) {
         m_arm = arm;
         m_endAngle = endAngle;
@@ -17,21 +27,21 @@ public class ArmRotate extends CommandBase {
 
     @Override
     public void initialize() {
-
+        DrivetrainConstants.rotationController.setGoal(m_endAngle);
     }
 
     @Override
     public void execute() {
-
+        m_arm.rotatePivot(DrivetrainConstants.rotationController.calculate(m_arm.getPivotEncoder(), m_endAngle));
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        m_arm.rotatePivot(0);
     }
 
     @Override
     public boolean isFinished() {
-        return m_arm.getPivotEncoder() > m_endAngle-.1 && m_arm.getPivotEncoder() < m_endAngle+.1;
+        return Math.abs(m_endAngle - m_arm.getPivotEncoder()) < 0.1;  //Checks for angle to be within 0.1 of endAngle
     }
 }
