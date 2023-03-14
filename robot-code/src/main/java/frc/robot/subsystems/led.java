6e5e71c00b62056a61b0f3fb726f4822458a3b3c
Tyler Thach <ttylerpro@gmail.com>
2023-03-14 19:28:23 -0400

    Configured led settings for robot, has not been tested and no guarantee it works
    Co-authored-by: shiruzo <shiruzo@users.noreply.github.com>

--- robot-code/src/main/java/frc/robot/subsystems/led.java
+++ robot-code/src/main/java/frc/robot/subsystems/led.java
@@ -7,86 +7,104 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Timer;
 public class led extends SubsystemBase {
   private AddressableLED m_led;
   private AddressableLEDBuffer m_ledBuffer;
+  private Timer timer;
   /** Creates a new led. */
   public led() {
     m_led = new AddressableLED(0);
     m_ledBuffer = new AddressableLEDBuffer(300); //300 should be the length
     m_led.setLength(m_ledBuffer.getLength());
     m_led.start(); //starts the leds
+     timer = new Timer();
   }
-//different LED settings for different occasions
+
+  public int getLength(){
+    return m_ledBuffer.getLength();
+  }
+  //-------------------------------------THEMES----------------------------------------------------------//
   public void resetled(){ //turns all the leds off
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       m_ledBuffer.setRGB(i, 0, 0, 0);
-      
    }
     m_led.setData(m_ledBuffer);
       }
-  
-  public void color(int r, int g, int b) {
-    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      m_ledBuffer.setRGB(i, r, g, b);
-    }
-    m_led.setData(m_ledBuffer);
-  }
 
-  public void yellow() {
+  public void yellow(){
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       m_ledBuffer.setRGB(i, 255, 255, 0);
-    }
-    m_led.setData(m_ledBuffer);
-  }
-
-  public void purple() {
-    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      m_ledBuffer.setRGB(i, 90, 52, 125);
-    }
-    m_led.setData(m_ledBuffer);
-  }
-  /* 
-
-  public void led1(){ //green
-    System.out.println("here"); 
-    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      m_ledBuffer.setRGB(i, 0, 255, 0);
    }
     m_led.setData(m_ledBuffer);
   }
 
-  public void led2(){ //red
+  public void purple(){
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      m_ledBuffer.setRGB(i, 255, 0, 0);
+      m_ledBuffer.setRGB(i, 0, 0, 0);
    }
-   m_led.setData(m_ledBuffer);
-    
-    
+    m_led.setData(m_ledBuffer);
   }
   
-  public void led3(int t){ //Red, green, blue cycle
-    if (t%3 == 0) {
-      m_ledBuffer.setRGB(t, 255, 0, 0);
-    }
-    else if (t%3 == 1) {
-      m_ledBuffer.setRGB(t, 0, 255, 0);
-    }
-    else {
-      m_ledBuffer.setRGB(t, 0, 0, 255);
-
+  //chantilly theme
+  public void ChantillyTheme(){ //Chantilly color pattern; purple and white
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      if(i % 2 == 0){
+        m_ledBuffer.setHSV(i, 136, 0, 60);
+      } else {
+        m_ledBuffer.setRGB(i, 75, 0, 130);
+      }
     }
     m_led.setData(m_ledBuffer);
-    
-    
-    m_led.start();
+  }
+
+  public void SixTwelveTheme(){ //612 color pattern; blue, yellow, and white
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      if(i % 3 == 0){
+        m_ledBuffer.setHSV(i, 136, 0, 60);
+      } else if(i % 3 == 1){
+        m_ledBuffer.setRGB(i, 255, 0, 255);
+      }else{
+        m_ledBuffer.setRGB(i, 0, 0, 255);
+      }
+    }
+    m_led.setData(m_ledBuffer);
   }
   
-  public void led4(int t, int r,int g, int b){ //rainbow gradient
-   m_ledBuffer.setRGB(t,r,g,b);
-   m_led.setData(m_ledBuffer);
+  //yellow sparkle
+  public void SparklePhase1(){
+    timer.start();
+    if (timer.get() <= 1){
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      if(i % 2 == 0){
+        m_ledBuffer.setRGB(i,255,255,0);
+        
+       }
+       else {
+        m_ledBuffer.setRGB(i,0,0,0);
+       }
+    }
   }
-*/
+    
+     if (timer.get() >= 1){
+      for (var i = 0; i < m_ledBuffer.getLength(); i++){
+        if (i % 2 == 1) {
+          m_ledBuffer.setRGB(i,255,255,0);
+        }
+        else {
+        m_ledBuffer.setRGB(i,0,0,0);
+        }
+      }
+      m_led.setData(m_ledBuffer);
+    }
+    if (timer.get() >= 2){
+      timer.reset();
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+  //ends here
+
   
 
   @Override
